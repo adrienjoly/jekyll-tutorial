7ca42540cb67dbd8f83baa116d7cee20ffa56192
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-07-17 17:01:35 +0200

    use my own algolia credentials

--- search.js
+++ search.js
@@ -1,8 +1,7 @@
 const search = instantsearch({
-  // TODO: enter our own algolia credentials here
-  appId: 'latency',
-  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-  indexName: 'instant_search',
+  appId: 'R6Y1H83FNX',
+  apiKey: 'b0f283176b810e5223f70e3ab3ac7c04',
+  indexName: 'albums',
   routing: true
 });
 
